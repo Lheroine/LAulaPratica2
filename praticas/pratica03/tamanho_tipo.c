f45e8f79b59d864b2cd9bcd8b2e9814dbad42823
Lheroine <Leonsofesi@gmail.com>
2025-09-10 21:53:33 -0300

    codigo da aula 4

---
+++ praticas/pratica03/tamanho_tipo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("O tipo 'char' ocupa %lu byte(s) na memória.\n", sizeof(char));
+    printf("O tipo 'int' ocupa %lu byte(s) na memória.\n", sizeof(int));
+    
+  return 0;
+}
